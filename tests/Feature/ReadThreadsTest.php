ad5257df65a6331a1e10c7f0aa97a49e0ef7063f
AlfaSchz <contact@alfasz.me>
2020-02-01 10:34:00 +0100

    Tests refactor

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -19,7 +19,7 @@ class ReadThreadsTest extends TestCase
     {
         parent::setUp();
 
-        $this->thread = factory('App\Thread')->create();
+        $this->thread = create('App\Thread');
     }
 
     /**
@@ -47,7 +47,7 @@ class ReadThreadsTest extends TestCase
      */
     public function a_user_can_read_replies_that_are_associates_with_a_thread()
     {
-        $reply = factory('App\Reply')->create(['thread_id' => $this->thread->id]);
+        $reply = create('App\Reply', ['thread_id' => $this->thread->id]);
 
         $this->get('/threads/' . $this->thread->id)
             ->assertStatus(200)
